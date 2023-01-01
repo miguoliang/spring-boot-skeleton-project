06d99e34abdeef4e05095fdbd03b08e24216a630
Mi Guoliang <boymgl@qq.com>
2023-01-01 08:59:54 +0800

    init

---
+++ biz-service/src/main/java/com/muchencute/bizservice/BizServiceApplication.java
@@ -0,0 +1,13 @@
+package com.muchencute.bizservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizServiceApplication {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(BizServiceApplication.class, args);
+  }
+}
---
+++ test-environment/src/main/java/com/muchencute/testenvironment/RestUnitTest.java
@@ -0,0 +1,31 @@
+package com.muchencute.testenvironment;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class RestUnitTest {
+
+  @RegisterExtension
+  final RestDocumentationExtension restDocumentationExtension = new RestDocumentationExtension("src/asciidoc");
+
+  protected MockMvc mockMvc;
+
+  @BeforeEach
+  void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .apply(documentationConfiguration(restDocumentation))
+            .build();
+  }
+}
