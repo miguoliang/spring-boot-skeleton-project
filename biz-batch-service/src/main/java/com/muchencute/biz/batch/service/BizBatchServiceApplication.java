816b5e7d386988acc6e0a4f3ffe4b43b4f646b63
Mi Guoliang <boymgl@qq.com>
2023-04-05 12:48:41 +0800

    feat: add a schedule job to remove unused job instances. feat: add a PostLaunchService to compose actions after launch.

--- biz-batch-service/src/main/java/com/muchencute/biz/batch/service/BizBatchServiceApplication.java
+++ biz-batch-service/src/main/java/com/muchencute/biz/batch/service/BizBatchServiceApplication.java
@@ -1,56 +1,31 @@
 package com.muchencute.biz.batch.service;
 
+import com.muchencute.biz.batch.service.service.PostLaunchService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.ExitStatus;
-import org.springframework.batch.core.explore.JobExplorer;
-import org.springframework.batch.core.launch.JobOperator;
-import org.springframework.batch.core.launch.NoSuchJobException;
-import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
-
-import java.util.Optional;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.muchencute")
 @EnableFeignClients
+@EnableScheduling
 @Slf4j
 public class BizBatchServiceApplication {
 
   public static void main(String[] args) {
 
     final var context = SpringApplication.run(BizBatchServiceApplication.class, args);
-    abandonRunningJobs(context);
+    System.out.println(">>> Datasource instance: " + context.getBean("dataSource"));
+    System.out.println(">>> TransactionManager instance: " + context.getBean("transactionManager"));
+    postLaunch(context);
   }
 
-  private static void abandonRunningJobs(ApplicationContext context) {
+  private static void postLaunch(ApplicationContext context) {
 
-    final var jobExplorer = (JobExplorer) context.getBean("jobExplorer");
-    final var jobOperator = (JobOperator) context.getBean("jobOperator");
-    final var jobRepository = (JobRepository) context.getBean("jobRepository");
-    log.info("启动时检查未完成的任务……");
-    jobExplorer.getJobNames().forEach(jobName -> {
-      log.info("检查任务 {} ...", jobName);
-      try {
-        jobOperator.getRunningExecutions(jobName).forEach(execution -> {
-          log.info("停止中 ... {}", execution);
-          Optional.ofNullable(jobExplorer.getJobExecution(execution)).ifPresent(jobExecution -> {
-            jobExecution.getStepExecutions().forEach(stepExecution -> {
-              log.info("停止 Step ...");
-              stepExecution.setStatus(BatchStatus.ABANDONED);
-              stepExecution.setExitStatus(ExitStatus.FAILED);
-              jobRepository.update(stepExecution);
-            });
-            jobExecution.setStatus(BatchStatus.ABANDONED);
-            jobExecution.setExitStatus(ExitStatus.FAILED);
-            jobRepository.update(jobExecution);
-            log.info("已停止！");
-          });
-        });
-      } catch (NoSuchJobException ignored) {
-      }
-    });
+    final var postLaunchService = context.getBean(PostLaunchService.class);
+    postLaunchService.abortRunningJobs();
+    postLaunchService.removeUnusedJobInstances();
   }
 }
---
+++ biz-batch-service/src/main/java/com/muchencute/biz/batch/service/service/PostLaunchService.java
@@ -0,0 +1,98 @@
+package com.muchencute.biz.batch.service.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobInstance;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.batch.core.launch.NoSuchJobException;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Slf4j
+public class PostLaunchService {
+
+  private final JobRepository jobRepository;
+
+  private final JobExplorer jobExplorer;
+
+  private final JobOperator jobOperator;
+
+  @Autowired
+  public PostLaunchService(JobRepository jobRepository, JobExplorer jobExplorer,
+                           JobOperator jobOperator) {
+
+    this.jobRepository = jobRepository;
+
+    this.jobExplorer = jobExplorer;
+
+    this.jobOperator = jobOperator;
+  }
+
+  public void removeUnusedJobInstances() {
+    // delete all job instances that are not running
+    jobRepository.getJobNames().forEach(
+        jobName -> jobExplorer.getJobInstances(jobName, 0, Integer.MAX_VALUE)
+            .forEach(jobInstance -> {
+                  final var isRunning = jobExplorer.getJobExecutions(jobInstance).stream()
+                      .anyMatch(JobExecution::isRunning);
+                  if (!isRunning) {
+                    log.info("删除 JobInstance {}-{}", jobInstance.getJobName(),
+                        jobInstance.getInstanceId());
+                    deleteJobExecutions(jobInstance);
+                    return;
+                  }
+                  log.info("JobInstance {}-{} 正在执行，不可删除！", jobInstance.getJobName(),
+                      jobInstance.getInstanceId());
+                }
+            ));
+  }
+
+  private void deleteJobExecutions(JobInstance jobInstance) {
+
+    jobExplorer.getJobExecutions(jobInstance).stream()
+        .filter(jobExecution -> !jobExecution.isRunning())
+        .forEach(jobExecution -> {
+          jobExecution.getStepExecutions().forEach(stepExecution -> {
+            log.info("删除 StepExecution {}-{}", stepExecution.getStepName(),
+                stepExecution.getId());
+            jobRepository.deleteStepExecution(stepExecution);
+          });
+          log.info("删除 JobExecution {}-{}", jobExecution.getJobInstance().getJobName(),
+              jobExecution.getId());
+          jobRepository.deleteJobExecution(jobExecution);
+        });
+  }
+
+  public void abortRunningJobs() {
+
+    log.info("启动时检查未完成的任务……");
+    jobExplorer.getJobNames().forEach(jobName -> {
+      log.info("检查任务 {} ...", jobName);
+      try {
+        jobOperator.getRunningExecutions(jobName).forEach(execution -> {
+          log.info("停止中 ... {}", execution);
+          Optional.ofNullable(jobExplorer.getJobExecution(execution)).ifPresent(jobExecution -> {
+            jobExecution.getStepExecutions().forEach(stepExecution -> {
+              log.info("停止 Step ...");
+              stepExecution.setStatus(BatchStatus.ABANDONED);
+              stepExecution.setExitStatus(ExitStatus.FAILED);
+              jobRepository.update(stepExecution);
+            });
+            jobExecution.setStatus(BatchStatus.ABANDONED);
+            jobExecution.setExitStatus(ExitStatus.FAILED);
+            jobRepository.update(jobExecution);
+            log.info("已停止！");
+          });
+        });
+      } catch (NoSuchJobException ignored) {
+      }
+    });
+  }
+}
